2d729084f65d5294c64b3047528bc7c6e5fe45f9
Khaera MB <khaera.belkadi@gmail.com>
2022-07-05 20:47:48 +0200

    update

--- scripts/pages/Profil.js
+++ scripts/pages/Profil.js
@@ -208,7 +208,6 @@ export default class Profil {
                document.querySelector("#btn").addEventListener("click", (e) =>{
                     e.preventDefault()
                     filter.display(result)
-                    filter.sortByTitle(result)
                })
     }
 
